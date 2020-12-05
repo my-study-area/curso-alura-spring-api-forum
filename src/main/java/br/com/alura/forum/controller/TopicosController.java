0a67cdce339b75db669137ad6bb6a6410326d5a8
Adriano Avelino <adriano-st777@hotmail.com>
2020-12-04 23:07:44 -0300

    atualiza o endpoint /topicos para mostrar o dados do banco de dados

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,21 +1,24 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.TopicoRepository;
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
 @RestController
 public class TopicosController {
 
+	@Autowired
+	private TopicoRepository topicoRepository;
+
 	@GetMapping("/topicos")
 	public List<TopicoDto> lista() {
-		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+		List<Topico> topicos = this.topicoRepository.findAll() ;
+		return TopicoDto.converter(topicos);
 	}
 }
